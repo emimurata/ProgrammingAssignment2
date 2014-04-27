12f89748a108f3b433ae20c23736481bc783a4bb
emimurata <emi.murata@upch.pe>
2014-04-27 17:25:04 -0500

    week3homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that saves a list with 4 other functions in 
+##  order to cache a matrix and its inverse, if it has been computed.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  inverse<-NULL
+  set_matrix <- function(y) { #this two functions overwrite the matrix stored and reset the value of the inverse to null
+    x <<- y
+    inverse <<- NULL #resets the inverse to null
+  }
+  get_matrix<- function() x #this two functions are for storing the matrix
+  set_inverse<-function(solve) inverse<<-solve #this two functions are for overwring the inversed matrix being stored if the original matrix change 
+  get_inverse<-function()inverse
+  list(set_matrix=set_matrix,
+       get_matrix=get_matrix,
+       set_inverse=set_inverse,
+       get_inverse=get_inverse)
 }
 
+## cacheInverse is a function that checks if the inverse of the matrix has been stored and prints it or computes it if needed
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  inverse <- x$get_inverse() #retrives the inversed matrix stored
+  if(!is.null(inverse)) { #checks if the matrix is there 
+    message("getting cached data")
+    return(inverse) #if its there, it is returned here
+  }
+  matrix <- x$get_matrix() #if it is not, the new matrix is set
+  inverse <- solve(matrix) #the new inversed matrix is calculated
+  x$set_inverse(inverse) #then stored
+  inverse #and printed
 }
